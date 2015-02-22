5cce937af064683a5f5075e265b8b907322b447e
Aaron Mell <aaronamell@gmail.com>
2015-02-21 19:00:28 -0600

    Initial Commit

---
+++ Log4NetDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Common.Logging;
+using log4net.Config;
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            //This configures the logger to use the Log4Net.config file. This must be done as early as possible in the lifetime of your application!
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log4Net.config")));
+
+            //Just a Simple Logging Statement
+            _logger.Info("Hello World");
+        }
+    }
+}
